ade9ce340fa17628bbb071eff43c6390231fdfe1
strauman <>
2007-07-28 19:32:14 +0000

    - added header with macro to detect RTEMS version (APIs keep changing :-()

---
+++ minversion.h
@@ -0,0 +1,15 @@
+#ifndef RTEMS_MINIMAL_VERSION_H
+#define RTEMS_MINIMAL_VERSION_H
+/* $Id$ */
+
+/* Macro to detect RTEMS version */
+#include <rtems.h>
+#include <rtems/system.h>
+
+#define RTEMS_ISMINVERSION(ma,mi,re) \
+	(    __RTEMS_MAJOR__  > (ma)	\
+	 || (__RTEMS_MAJOR__ == (ma) && __RTEMS_MINOR__  > (mi))	\
+	 || (__RTEMS_MAJOR__ == (ma) && __RTEMS_MINOR__ == (mi) && __RTEMS_REVISION__ >= (re)) \
+    )
+
+#endif
